7cf014b13bf10e50ae144db7439fec1b0b496ed7
Matheus <math.olivribeiro@gmail.com>
2025-04-04 20:58:11 -0300

    Exer

--- condicionais.js
+++ condicionais.js
@@ -1,38 +1,139 @@
-// let numero = prompt("Digite um número"); exer1
-
-// if (numero > 0) {
-//     console.log("Positivo");
-
-// }
-// else{
-//     if (numero <0) {
-//         console.log("Negativo");
-//     }
-//     else {
-//         console.log("É zero")
-//     }
-// }
+function exerc01() {
 
 
-// let numero = prompt("Digite sua idade"); exer 2
+    let numero = prompt("Digite um número");
 
-// if (numero > 18) {
-//     console.log("Voce é de maior");
-// }
-//     else{
-//              if (numero < 18) {
-//                  console.log("Voce e de menor");
-//          }
-//             else {
-//                 console.log("É de maior")
-//             }
-// }
+    if (numero > 0) {
+        console.log("Positivo");
 
+    }
+    else {
+        if (numero < 0) {
+            console.log("Negativo");
+        }
+        else {
+            console.log("É zero")
+        }
+    }
 
-let numero = prompt("Digite um número"); 
-
-if (numero % 2 === 0) {
-    console.log("O número " + numero + " é par.");
-} else {
-    console.log("O número " + numero + " é ímpar.");
 }
+
+function exerc02() {
+    let numero = prompt("Digite sua idade");
+
+    if (numero > 18) {
+        console.log("Voce é de maior");
+    }
+    else {
+        if (numero < 18) {
+            console.log("Voce e de menor");
+        }
+        else {
+            console.log("É de maior")
+        }
+    }
+
+}
+
+function exerc03() {
+
+    let numero = prompt("Digite um número");
+
+    if (numero % 2 === 0) {
+        console.log("O número " + numero + " é par.");
+    } else {
+        console.log("O número " + numero + " é ímpar.");
+    }
+
+}
+
+function exerc04() {
+
+    let numero = prompt("Insira a nota");
+
+    if (numero >= 60) {
+        console.log("Aprovado");
+    } else {
+        if (numero <= 60) {
+            console.log("Reprovado")
+        }
+    }
+
+}
+
+function exerc05() {
+    let numero = prompt("Digite um número");
+
+    if (numero > 0) {
+        console.log("Positivo");
+
+    }
+    else {
+        if (numero < 0) {
+            console.log("Negativo");
+        }
+
+    }
+}
+
+function exerc06() {
+    let numero = prompt("Digite sua idade");
+
+    if (numero >= 18) {
+        alert ("adulto");
+
+    } else {
+        if (numero >= 13) {
+            alert ("adolecente");
+        } else {
+                alert ("criança");
+            
+        }
+
+    }
+
+} 
+
+function exerc07 (){
+let numero1 = prompt ("Digite o primeiro número:");
+
+let operacao = prompt("Digite a operação (+, -, *, /):");
+
+let numero2 = prompt("Digite o segundo número:");
+
+
+if (operacao === "+") {
+    resultado = numero1 + numero2;
+} else if (operacao === "-") {
+    resultado = numero1 - numero2;
+} else if (operacao === "*") {
+    resultado = numero1 * numero2;
+} else if (operacao === "/") {
+    
+}
+alert("O resultado é: " + resultado);
+   
+}
+exerc07 ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
