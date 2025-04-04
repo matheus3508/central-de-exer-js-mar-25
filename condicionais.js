d4565ef065463060c74dd19bee6c6ce8b0f826c1
Matheus <math.olivribeiro@gmail.com>
2025-04-04 19:08:17 -0300

    Exer

---
+++ condicionais.js
@@ -0,0 +1,38 @@
+// let numero = prompt("Digite um número"); exer1
+
+// if (numero > 0) {
+//     console.log("Positivo");
+
+// }
+// else{
+//     if (numero <0) {
+//         console.log("Negativo");
+//     }
+//     else {
+//         console.log("É zero")
+//     }
+// }
+
+
+// let numero = prompt("Digite sua idade"); exer 2
+
+// if (numero > 18) {
+//     console.log("Voce é de maior");
+// }
+//     else{
+//              if (numero < 18) {
+//                  console.log("Voce e de menor");
+//          }
+//             else {
+//                 console.log("É de maior")
+//             }
+// }
+
+
+let numero = prompt("Digite um número"); 
+
+if (numero % 2 === 0) {
+    console.log("O número " + numero + " é par.");
+} else {
+    console.log("O número " + numero + " é ímpar.");
+}
--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-alert("Hello world na tela");
+// alert("Hello world na tela");
 
-console.log("Hello World no console");
+// console.log("Hello World no console");
+
+let nome = "Matheus";
+const idade = 22;  
+var cidade = "Ribeirão Pires"; 
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+console.log("Tenho " + idade +  " anos");
+console.log("Minha cidade é: " + cidade); //Concatenação
+
+console.log(`Meu nome é: ${nome}`); //Interpolação
+console.log(`Minha idade é: ${idade}`); //Interpolação
+console.log(`Meu minha cidade é: ${cidade}`); //Interpolação
+
+console.log(typeof idade)
+
+let n1 =10;
+let n2 = 10;
+
+let soma = n1 + n2;
+
+console.log(soma);
